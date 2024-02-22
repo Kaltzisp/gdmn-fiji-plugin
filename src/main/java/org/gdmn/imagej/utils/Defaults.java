c3d39f0fd2aa8e045f3cf3acb3f783efa3b20c3f
Peter Kaltzis <peterkaltzis@outlook.com>
2024-02-22 16:25:13 +1100

    added custom defaults

---
+++ src/main/java/org/gdmn/imagej/utils/Defaults.java
@@ -0,0 +1,63 @@
+package org.gdmn.imagej.utils;
+
+import ij.IJ;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Defaults {
+    static Path tmpPath = Paths.get(IJ.getDirectory("temp") + "FIJI_DEFAULTS");
+
+    public static String get(String key) {
+        if (!Defaults.hasDefaultsFile()) {
+            return "";
+        }
+        try {
+            List<String> lines = Files.readAllLines(tmpPath);
+            for (String store : lines) {
+                if (key.equals(store.split("=")[0])) {
+                    return store.split("=")[1];
+                }
+            }
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+        return "";
+    }
+
+    public static void set(String key, String value) {
+        if (!Defaults.hasDefaultsFile()) {
+            try {
+                Files.write(tmpPath, Collections.singleton(key + "=" + value));
+            } catch (IOException e) {
+                System.err.println(e);
+            }
+        } else {
+            try {
+                List<String> lines = Files.readAllLines(tmpPath);
+                List<String> modifiedLines = new ArrayList<>();
+                for (String store:lines) {
+                    IJ.log(store);
+                    if (key.equals(store.split("=")[0])) {
+                        store = key + "=" + value;
+                    }
+                    modifiedLines.add(store);
+                }
+                Files.write(tmpPath, modifiedLines);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private static boolean hasDefaultsFile() {
+        File f = new File(tmpPath.toUri());
+        return (f.exists() && !f.isDirectory());
+    }
+
+}
