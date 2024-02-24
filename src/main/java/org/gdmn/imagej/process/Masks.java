f094d982a1a9f8249c2448e5df6b593f931cc420
Peter Kaltzis <peterkaltzis@outlook.com>
2024-02-24 19:17:19 +1100

    updated Masks command

--- src/main/java/org/gdmn/imagej/process/Masks.java
+++ src/main/java/org/gdmn/imagej/process/Masks.java
@@ -3,7 +3,7 @@ package org.gdmn.imagej.process;
 import java.util.List;
 
 import org.gdmn.imagej.utils.BatchCommand;
-import org.gdmn.imagej.utils.FileFinder;
+import org.gdmn.imagej.utils.Filer;
 import org.scijava.command.Command;
 import org.scijava.plugin.Plugin;
 
@@ -11,8 +11,11 @@ import org.scijava.plugin.Parameter;
 import org.scijava.ItemVisibility;
 import org.scijava.widget.Button;
 
+import ij.IJ;
 import ij.ImagePlus;
+import ij.gui.Roi;
 import ij.plugin.filter.RankFilters;
+import ij.plugin.filter.ThresholdToSelection;
 import ij.process.ImageProcessor;
 
 import inra.ijpb.morphology.Morphology;
@@ -20,55 +23,49 @@ import inra.ijpb.morphology.Strel;
 
 @Plugin(type = Command.class, menuPath = "2D Macro Tool>Create Masks")
 public class Masks extends BatchCommand {
-    
+
     @Parameter(visibility = ItemVisibility.MESSAGE)
     private String maskMessage = "<h2 style='width: 500px'>Create tissue masks</h2>";
 
     @Parameter(label = "Multiplier:")
     private float multiplier = 4;
 
-    @Parameter(label = "Median Radius:")
+    @Parameter(label = "Median radius:")
     private float medianRadius = 20;
 
-    @Parameter(label = "Closing Radius:")
+    @Parameter(label = "Closing radius:")
     private int closingRadius = 4;
+
+    @Parameter(label = "Output filename")
+    private String outputFile = "mask_myo.tif";
 
     @Parameter(label = "Run", callback = "execute")
     private Button runButton;
 
     public void execute() {
-        List<String> filePaths = FileFinder.getFiles(this.selectedDir, this.filePattern);
+        List<String> filePaths = Filer.getFiles(this.selectedDir, this.filePattern);
         for (String filePath : filePaths) {
+            IJ.showProgress(closingRadius);
             ImagePlus imp = new ImagePlus(filePath);
             ImageProcessor ip = imp.getProcessor();
+
+            // Creating mask.
             ip.multiply(multiplier);
             new RankFilters().rank(ip, medianRadius, RankFilters.MEDIAN);
-            ImageProcessor resultIP = Morphology.closing(ip, Strel.Shape.DISK.fromRadius(closingRadius));
-            imp = new ImagePlus("Result", resultIP);
-            imp.show();
+            ip = Morphology.closing(ip, Strel.Shape.DISK.fromRadius(closingRadius));
+            ip.autoThreshold();
+            imp.setProcessor(ip);
+
+            // Drawing selection.
+            ip.setThreshold(255, 255, ImageProcessor.NO_LUT_UPDATE);
+            Roi roi = ThresholdToSelection.run(imp);
+            imp.setRoi(roi);
+
+            // Saving mask.
+            Filer.save(imp, filePath, "masks", outputFile);
+            imp.close();
         }
 
     }
 
-
 }
-
-// // Getting input.
-// input = split(getArgument(), ",");
-// path = input[0];
-// type = input[1];
-// multiplier = parseFloat(input[2]);
-// radius = parseFloat(input[3]);
-// closer = parseFloat(input[4]);
-// output = input[5];
-
-// // Creating mask.
-// open(path+"channels/"+type+".tif");
-// run("Multiply...", "value="+multiplier);
-// run("Median...", "radius="+radius);
-// run("Morphological Filters", "operation=Closing element=Square radius="+closer);
-// run("Convert to Mask");
-// run("Create Selection");
-// save(path+"masks/mask_"+output+".tif");
-// close(type+".tif");
-// close(type+"-Closing");
