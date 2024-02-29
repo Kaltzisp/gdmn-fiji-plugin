0147b9a3c608dc0243841cbc2fcc45a680c366dd
Peter Kaltzis <peterkaltzis@outlook.com>
2024-02-29 18:14:05 +1100

    added trabecular capability to CustomMask

--- src/main/java/org/gdmn/imagej/process/CustomMask.java
+++ src/main/java/org/gdmn/imagej/process/CustomMask.java
@@ -11,8 +11,11 @@ import org.scijava.widget.Button;
 
 import ij.IJ;
 import ij.ImagePlus;
+import ij.gui.Roi;
 import ij.gui.Toolbar;
 import ij.gui.WaitForUserDialog;
+import ij.plugin.filter.ThresholdToSelection;
+import ij.process.ImageProcessor;
 
 @Plugin(type = Command.class, menuPath = "2D Macro Tool>Custom Mask")
 public class CustomMask extends BatchCommand {
@@ -20,24 +23,20 @@ public class CustomMask extends BatchCommand {
     @Parameter(visibility = ItemVisibility.MESSAGE)
     private String header = "<h2 style='width: 500px'>Create custom mask</h2>";
 
-    @Parameter(label = "Template type", choices = { "Entire ROI", "Channel",
-            "Mask" }, callback = "updateTemplateTissues")
+    @Parameter(label = "Template type", persist = false, choices = { "Entire ROI", "Channel", "Mask" })
     private String templateType = Defaults.get("templateType", "Mask");
 
-    @Parameter(label = "Template tissue", choices = { "myo", "endo", "marker", "nuclei", "-" })
-    private String templateTissue = Defaults.get("templateTissue", "myo.tif");
+    @Parameter(label = "Template tissue", persist = false, choices = { "myo", "endo", "marker", "nuclei", "-" })
+    private String templateTissue = Defaults.get("templateTissue", "myo");
 
-    @Parameter(label = "Output mask name")
+    @Parameter(label = "Output mask name", persist = false)
     private String outputMask = Defaults.get("outputMask", "mask_myo_compact.tif");
+
+    @Parameter(label = "Generate trabecular mask", persist = false)
+    private Boolean createTrabecularMask = Boolean.parseBoolean(Defaults.get("createTrabecularMask", "true"));
 
     @Parameter(label = "Run", callback = "runAll")
     private Button runButton;
-
-    public void updateTemplateTissues() {
-        if (this.templateType.equals("Entire ROI")) {
-            this.templateTissue = "-";
-        }
-    }
 
     public void process(String roiPath) {
         String filePath = roiPath;
@@ -47,19 +46,53 @@ public class CustomMask extends BatchCommand {
             filePath = Filer.getPath(roiPath, "masks", "mask_" + this.templateTissue + ".tif");
         }
 
-        // Opening template and requesting drawing.
+        // Opening drawing template and requesting drawing.
         ImagePlus imp = new ImagePlus(filePath);
+        imp.killRoi();
         imp.show();
         IJ.setTool(Toolbar.POLYGON);
         WaitForUserDialog dialog = new WaitForUserDialog("Draw mask", "Select mask then hit OK.");
         dialog.show();
 
-        // Copying mask to new image.
-        ImagePlus mask = new ImagePlus("Mask", imp.createRoiMask());
+        ImagePlus mask;
+        ImageProcessor maskIp;
+
+        if (!templateTissue.equals("-")) {
+            // If template tissue, combine template with selection.
+            mask = new ImagePlus(Filer.getPath(roiPath, "masks", "mask_" + templateTissue + ".tif"));
+            Roi roi = imp.getRoi();
+            imp.close();
+            maskIp = mask.getProcessor();
+            mask.setRoi(roi);
+            maskIp.setColor(0);
+            maskIp.fillOutside(roi);
+        } else {
+            // If no template tissue, save the entire selection.
+            mask = new ImagePlus("Mask", imp.createRoiMask());
+            imp.close();
+            maskIp = mask.getProcessor();
+        }
+
+        // Redrawing ROI.
+        maskIp.setThreshold(255, 255, ImageProcessor.NO_LUT_UPDATE);
+        mask.setRoi(ThresholdToSelection.run(mask));
+
+        // Saving and closing.
         Filer.save(mask, roiPath, "masks", outputMask);
-        imp.close();
+
+        if (createTrabecularMask) {
+            Roi roi = mask.getRoi();
+            ImagePlus trabecularMask = new ImagePlus(
+                    Filer.getPath(roiPath, "masks", "mask_" + templateTissue + ".tif"));
+            ImageProcessor trabIp = trabecularMask.getProcessor();
+            trabIp.fill(roi);
+            trabIp.setThreshold(255, 255, ImageProcessor.NO_LUT_UPDATE);
+            trabecularMask.setRoi(ThresholdToSelection.run(trabecularMask));
+            Filer.save(trabecularMask, roiPath, "masks", "mask_myo_trabecular.tif");
+            trabecularMask.close();
+        }
+
         mask.close();
 
     }
-
 }
