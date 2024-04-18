644938c47d84171eb88efc52238ae892656563ff
Peter Kaltzis <peterkaltzis@outlook.com>
2024-04-18 23:43:14 +1000

    updated CreateFolders command

--- src/main/java/org/gdmn/imagej/process/CreateFolders.java
+++ src/main/java/org/gdmn/imagej/process/CreateFolders.java
@@ -5,7 +5,6 @@ import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import org.gdmn.imagej.utils.BatchCommand;
-import org.gdmn.imagej.utils.Defaults;
 import org.scijava.ItemVisibility;
 import org.scijava.command.Command;
 import org.scijava.plugin.Menu;
@@ -23,30 +22,26 @@ import org.scijava.widget.Button;
 })
 public class CreateFolders extends BatchCommand {
 
-    @Parameter(visibility = ItemVisibility.MESSAGE)
-    private String header = "<h2 style='width: 500px'>Create folder hierarchy</h2>";
+    @Parameter(visibility = ItemVisibility.MESSAGE, persist = false)
+    private String header = "<h2 style='width: 500px;'>Create folder hierarchy</h2>";
 
-    @Parameter(visibility = ItemVisibility.MESSAGE)
-    private String info = "Creates a folder hierarchy from a set of image files. "
-            + "Images should all be within the same folder and named in the style below:<br><br>"
-            + "<b>IMAGING_INFO-STAGE-TEST_GROUP-EMBRYO_NUMBER-REGION-SECTION.tif</b><br>"
-            + "PK_IF5_Batch_1-E11.5-KLF8_WT-Embryo_32-LV-001.tif";
-
-    @Parameter(label = "Delimiter")
-    private String delimiter = Defaults.get("delimiter", "-");
+    @Parameter(visibility = ItemVisibility.MESSAGE, persist = false)
+    private String info = "<p style='width: 500px;'>"
+            + "Creates the required folder hierarchy for the plugin from a set of image files. "
+            + "Images should all be within the same folder and named in the style below.<br><br>"
+            + "<b>Style: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</b> IMAGING_INFO-STAGE-TEST_GROUP-EMBRYO_NUMBER-REGION-SECTION.tif<br>"
+            + "<b>Example:</b> PK_IF5_Batch_1-E11.5-KLF8_WT-Embryo_32-LV-001.tif</p>";
 
     @Parameter(label = "Run", callback = "runAll")
     private Button runButton;
 
-    /**
-     * Builds a folder hierarchy based on the images present.
-     */
+    /** Builds a folder hierarchy based on the images present. */
     public void process(String basePath) {
 
         // Getting delimited URI and building path.
         String currentDir = "";
         String filePath = this.filePath.substring(0, this.filePath.lastIndexOf("."));
-        String[] dirPath = filePath.split(delimiter);
+        String[] dirPath = filePath.split("-");
         for (int i = 0; i < dirPath.length; i++) {
             currentDir = Paths.get(currentDir, dirPath[i]).toString();
             File dir = new File(currentDir);
